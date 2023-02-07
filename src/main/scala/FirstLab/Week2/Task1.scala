d26585cd275ba9607122e13c0f302db3c4007136
butnaruv <butnaru.valeria05@gmail.com>
2023-02-07 21:08:07 +0200

    Add comment with task requirement

--- src/main/scala/FirstLab/Week2/Task1.scala
+++ src/main/scala/FirstLab/Week2/Task1.scala
@@ -2,17 +2,19 @@ package FirstLab.Week2
 
 import scala.math.sqrt
 
+//Task1 -> Test if a number is prime
 object Task1 extends App {
   {
     println(checkPrimeNumber(13))
   }
-  def checkPrimeNumber(number:Int): Boolean ={
+
+  def checkPrimeNumber(number: Int): Boolean = {
     var isPrime = true
-    if (number <= 1 ) {
+    if (number <= 1) {
       isPrime = false
-    }else{
+    } else {
       for (i <- 2 to sqrt(number).toInt) {
-        if(number % i == 0) isPrime = false;
+        if (number % i == 0) isPrime = false;
       }
     }
     return isPrime
--- src/main/scala/FirstLab/Week2/Task2.scala
+++ src/main/scala/FirstLab/Week2/Task2.scala
@@ -1,11 +1,14 @@
 package FirstLab.Week2
+
 import scala.math.pow
 
+//Task2 -> Compute are of a cylinder, given the height and the radius.
 object Task2 extends App {
-   {
-      print(cylinderArea(3,4))
+  {
+    print(cylinderArea(3, 4))
   }
-  def cylinderArea( height:Int, radius:Int ) : Double = {
+
+  def cylinderArea(height: Int, radius: Int): Double = {
     @inline val Pi = java.lang.Math.PI
     val baseArea = Pi * pow(radius, 2)
     val lateralArea = 2 * Pi * radius * height;
