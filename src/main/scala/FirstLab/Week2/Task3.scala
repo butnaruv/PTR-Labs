0c6f4b1d8bd3e919238d627b29c9578593fe85a5
butnaruv <butnaru.valeria05@gmail.com>
2023-02-08 12:53:21 +0200

    Add task3, week 2

---
+++ src/main/scala/FirstLab/Week2/Task3.scala
@@ -0,0 +1,22 @@
+package FirstLab.Week2
+
+//Task 3 -> reverse a list
+object Task3 extends App {
+  {
+    val myList: Array[Int] = Array(1, 2, 4, 8, 4)
+    reverseList(myList: _*)
+  }
+
+  def reverseList(myList: Int*): Unit = Array[Int] {
+    var reversedList = new Array[Int](myList.length)
+    var s = myList.length - 1
+    for (i <- 0 until myList.length) {
+      reversedList(s) = myList(i)
+      s = s - 1
+    }
+    return for (i <- reversedList) {
+      print(i)
+    }
+  }
+
+}
