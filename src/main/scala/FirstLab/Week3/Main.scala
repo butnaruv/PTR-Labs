a2e37d0c63543182eef66291433bba620dfb3e66
butnaruv <butnaru.valeria05@gmail.com>
2023-02-16 18:10:17 +0200

    Add minimal task4, week3

--- src/main/scala/FirstLab/Week3/Main.scala
+++ src/main/scala/FirstLab/Week3/Main.scala
@@ -6,12 +6,19 @@ object Main {
     val system = ActorSystem("PrinterSystem")
     val printer = system.actorOf(MyFirstActor.props, "printer")
     val printerModifier = system.actorOf(AdjustActor.props, "printerModifier")
+    val averageComputer = system.actorOf(ComputeAverageActor.props , "averageComputer")
 
-    printer ! "Hello!"
-    printer ! 445
-    printerModifier !"Hello"
-    printerModifier ! 5
-    printerModifier ! List(1, 3)
+//    printer ! "Hello!"
+//    printer ! 445
+//    printerModifier !"Hello"
+//    printerModifier ! 5
+//    printerModifier ! List(1, 3)
+    averageComputer ! 0
+    averageComputer ! 10
+    averageComputer ! 10
+    averageComputer ! 10
+
+
   }
 }
 
