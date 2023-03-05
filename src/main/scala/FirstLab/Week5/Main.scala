f802c7e547a843d57862aa2407dfecc64f6a576e
butnaruv <butnaru.valeria05@gmail.com>
2023-03-05 21:49:27 +0200

    Add minimal task2, week5

--- src/main/scala/FirstLab/Week5/Main.scala
+++ src/main/scala/FirstLab/Week5/Main.scala
@@ -1,29 +1,46 @@
 package FirstLab.Week5
 
 import java.net.{HttpURLConnection, URL}
+import scala.collection.mutable.ArrayBuffer
 
 object Main extends App {
+
   {
+
     println("---Minimal Tasks---")
     MinimalTask1()
+    MinimalTask2()
 
   }
+  private var body = ""
 
   def MinimalTask1(): Unit = {
     val url = new URL("https://quotes.toscrape.com/")
     val connection = url.openConnection().asInstanceOf[HttpURLConnection]
     connection.setRequestMethod("GET")
-
     println(s"Response status code: ${connection.getResponseCode}")
     println("Response headers:")
     connection.getHeaderFields.forEach((key, value) => println(s"$key: $value"))
-
-
-    val body = scala.io.Source.fromInputStream(connection.getInputStream).mkString
+    body = scala.io.Source.fromInputStream(connection.getInputStream).mkString
     println(s"Response body: $body")
-
-
     connection.disconnect()
+  }
 
+  def MinimalTask2(): Unit = {
+
+    val listOfMaps = ArrayBuffer[Map[String, String]]()
+    val selectGeneralBlock = "<div class=\"quote\"[\\s\\S]*?<\\/div>".r
+    val selectQuotes = "<span class=\"text\" itemprop=\"text\">(“.*)<\\/span>".r
+    val selectAuthors = "itemprop=\"author\">(.*)<\\/small>".r
+    val selectTags = "<a class=\"tag\".*?>(.*)<\\/a>".r
+
+    val listOfQuotes = selectGeneralBlock.findAllIn(body).toList
+    for(element <- listOfQuotes){
+      val quote = selectQuotes.findFirstMatchIn(element).map(_.group(1)).fold("")(_.toString)
+      val author = selectAuthors.findFirstMatchIn(element).map(_.group(1)).fold("")(_.toString)
+      val tags = selectTags.findAllMatchIn(element).map(_.group(1)).mkString("; ")
+      listOfMaps.addOne(Map("quote" -> quote, "author" -> author, "tags" -> tags))
+    }
+    for(map <- listOfMaps) println(map)
   }
 }
